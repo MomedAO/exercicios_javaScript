8ac80afa54403dce558124c0be895388d6ac7746
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2021-07-18 18:25:29 +0200

    Dinheiro na forma correta

---
+++ restoDaDivisao.js
@@ -0,0 +1,11 @@
+/**
+ * 04) Crie uma função que irá receber dois valores, o dividendo 
+ * e o divisor. A função deverá imprimir o resultado e o resto da 
+ * divisão destes dois valores.
+ */
+
+//funcao literal
+function resto(dividendo, divisor){
+    console.log(`Resto da divisao = ${dividendo % divisor}`)
+}
+resto(4, 2)
