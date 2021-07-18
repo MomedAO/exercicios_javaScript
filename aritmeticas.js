0f330e72ad82e7dd80eb0e3fb53949d443a41b5b
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2021-07-18 15:50:21 +0200

    operacoes aritmeticas

---
+++ aritmeticas.js
@@ -0,0 +1,15 @@
+/**
+ * 01) Crie uma função que dado dois valores (passados como parâmetros) 
+ * mostre no console a soma, subtração, multiplicação e divisão desses 
+ * valores. 
+ */
+
+//funcao literal
+function aritmeticas(num1, num2){
+    console.log(`Soma = ${num1 + num2}`)
+    console.log(`Subtracao = ${num1 - num2}`)
+    console.log(`Multiplicacao = ${num1 * num2}`)
+    console.log(`Divisao = ${num1 / num2}`)
+}
+
+aritmeticas(2, 4)
