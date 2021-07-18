b5e655da6e60e650b1b1f8dd225e288e788e12ca
Momed Abdala Ossufo <momedabdalaa@gmail.com>
2021-07-18 16:40:51 +0200

    Exponencicao

---
+++ exponenciacao.js
@@ -0,0 +1,10 @@
+/**
+ * 03) Crie uma função que recebe dois parâmetros, 
+ * base e expoente, e retorne a base elevada ao expoente.
+ */
+
+function exponenciacao(num, expoente){
+    console.log(`${num} ^ ${expoente} = ${Math.pow(num, expoente)}`)
+}
+
+exponenciacao(2, 10)
